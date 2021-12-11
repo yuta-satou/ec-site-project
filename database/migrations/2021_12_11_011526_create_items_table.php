f948544d76fcbea22a8d3a8aac4cd4ac2ce99c51
yuta-satou <yutaw21t@gmail.com>
2021-12-11 10:36:04 +0900

    空のItems作成

---
+++ database/migrations/2021_12_11_011526_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('explanation');
+            $table->integer('money');
+            $table->string('image');
+            $table->integer('stock');
+            // $table->integer('time_sale_id');
+            // $table->integer('genre_id');
+            // $table->integer('size_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
