07a3808bbf229eb07d10a47f1f7ac02e6cae263a
yuta-satou <yutaw21t@gmail.com>
2021-12-26 20:11:42 +0900

    footer作成

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="center">Made with <i class="fa fa-heart"></i> by <a href="https://revolthemes.net/" target="_blank">Revolthemes</a>. All Rights Reserved</p>
+
+            </div>
+        </div>
+    </div>
+</footer>
